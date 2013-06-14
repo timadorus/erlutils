e6a731de65b092fe1b00494262c4a76eaa848dcf
Lutz Behnke <lutz.behnke@gmx.de>
2013-06-14 09:21:03 +0200

    add mock server

---
+++ src/teu_async_mock.erl
@@ -0,0 +1,137 @@
+%%% -------------------------------------------------------------------
+%%% Author  : sage
+%%% Description :
+%%%
+%%% Created : 12.06.2013
+%%% -------------------------------------------------------------------
+-module(teu_async_mock).
+
+-behaviour(gen_server).
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+-include_lib("eunit/include/eunit.hrl").
+
+%% --------------------------------------------------------------------
+%% External exports
+%% --------------------------------------------------------------------
+-export([start_link/1, last_message/1, stop/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {lastMessage}).
+
+%% ====================================================================
+%% External functions
+%% ====================================================================
+
+%% start_link/1
+%% --------------------------------------------------------------------
+%% @doc start the mock.
+%% if Register is set to 'register' the process will register locally with the name 'quperl_control'.
+%% @end
+%% --------------------------------------------------------------------
+%% FIXME: do a proper error/exception handling if the value of the option does 
+%%        not make sense (e.g. something other than an atom)
+-spec start_link(Options::[{register, atom()}]) -> {ok, Pid::pid()} | ignore | {error, Error::term()}.
+start_link(Options) ->
+	RegisterName = teu_application:opt(register, Options),
+    case RegisterName of
+		true ->      gen_server:start_link({local, ?MODULE}, ?MODULE, [], []);
+		false ->     gen_server:start_link(?MODULE, [], []); 
+        RegisterName ->  
+           if 
+            is_atom(RegisterName) -> gen_server:start_link({local, RegisterName}, ?MODULE, [], []);
+            true -> gen_server:start_link(?MODULE, [], [])
+		   end
+    end.
+
+
+-spec last_message(Pid::pid()) -> term().
+last_message(Pid) ->
+    gen_server:call(Pid, get_last).
+
+
+-spec stop(Pid::pid()) -> ok.
+stop(Pid) -> gen_server:cast(Pid, stop).
+
+
+%% ====================================================================
+%% Server functions
+%% ====================================================================
+
+%% --------------------------------------------------------------------
+%% Function: init/1
+%% Description: Initiates the server
+%% Returns: {ok, State}          |
+%%          {ok, State, Timeout} |
+%%          ignore               |
+%%          {stop, Reason}
+%% --------------------------------------------------------------------
+init([]) ->
+    {ok, #state{}}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_call/3
+%% Description: Handling call messages
+%% Returns: {reply, Reply, State}          |
+%%          {reply, Reply, State, Timeout} |
+%%          {noreply, State}               |
+%%          {noreply, State, Timeout}      |
+%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_call(get_last, _From, State) ->
+    ?debugFmt("quperl_client_mock:handle_call called for: get_last",[]),
+    {reply, State#state.lastMessage, State};
+
+handle_call(Request, _From, State) ->
+    ?debugFmt("unknown message: ~p", [Request]),
+    {reply, {unknown_message, Request}, error, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_cast/2
+%% Description: Handling cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_cast(stop, State) ->
+    {stop, normal, State};
+
+handle_cast(Msg, State) ->
+    ?debugFmt("have seen message: ~p",[Msg]),
+    {noreply, State#state{lastMessage = Msg}}.
+
+
+%% --------------------------------------------------------------------
+%% Function: handle_info/2
+%% Description: Handling all non call/cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: terminate/2
+%% Description: Shutdown the server
+%% Returns: any (ignored by gen_server)
+%% --------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%% --------------------------------------------------------------------
+%% Func: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%% --------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+
---
+++ test/teu_async_mock_tests.erl
@@ -0,0 +1,68 @@
+%%
+%% @doc TODO: Add description to teu_nodes_tests
+%%
+%% @author sage
+%%
+%% @copyright 2009-2012 Timadorus Project
+
+-module(teu_nodes_tests).
+
+%%
+%% Include files
+%%
+-include_lib("eunit/include/eunit.hrl").
+
+%%
+%% Exported Functions
+%%
+-export([]).
+
+%%
+%% Fixtures
+%%
+
+info_test_() ->
+    { setup, fun() -> ok end,
+      fun() -> ?debugFmt("~n############################################~n      starting ~p~n############################################~n  ", [?MODULE]) end }.
+
+initial_test_() ->
+    { "some initial tests",
+      setup,
+
+      fun() ->
+              %%     application:start(sasl),
+              ok
+      end,
+
+      fun(_Args) ->
+              %%     application:stop(sasl),
+              ok
+      end,
+      fun(_Foo) -> [
+                    ?_test(test_send_message()),
+                    ?_test(test_register())
+                   ]
+      end }.
+
+
+
+%%
+%% Local Functions
+%%
+test_send_message() ->
+	Res = teu_async_message:start_link([]),
+	?assertMatch({ok, _Pid}, Res),
+	{ok, Pid} = Res,
+	
+	?assertMatch(undefined, whereis(true)),
+	?assertMatch(undefined, whereis(teu_async_mock)),
+	
+	gen_server:cast(Pid, test_message),
+	
+	?assertEqual(test_message, teu_async_message:last_message(Pid)),
+	
+    ok.
+
+test_register() ->
+	
+    ok.
