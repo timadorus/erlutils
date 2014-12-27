4f1e37a3cbc975c7c92b6a60b08fc6c1c223461b
Lutz Behnke <lutz.behnke@gmx.de>
2014-12-27 07:35:02 +0100

    add function to wait for specific message

--- src/teu_async_mock.erl
+++ src/teu_async_mock.erl
@@ -15,14 +15,18 @@
 %% --------------------------------------------------------------------
 %% External exports
 %% --------------------------------------------------------------------
--export([start_link/1, last_message/1, message_stack/1, stop/1]).
+-export([start_link/1, start/1, stop/1, 
+		 last_message/1, message_stack/1, wait_for_msg/1, wait_for_msg/2, 
+		 is_verbose/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(state, {lastMessage                 :: term(), 
-                messageStack = []           :: [term()], 
-                opt_verbose = false         :: boolean()}).
+-record(state, {lastMessage                        :: term(), 
+                messageStack = []                  :: [term()], 
+                opt_verbose = false                :: boolean(),
+				observer = none                    :: pid() | none,
+				observerPred = fun(_) -> true end  :: fun()}).
 
 %% ====================================================================
 %% External functions
@@ -49,13 +53,36 @@ start_link(Options) ->
 		   end
     end.
 
+%% start/1
+%% --------------------------------------------------------------------
+%% @doc start the mock.
+%% if Register is set to 'register' the process will register locally with the name 'quperl_control'.
+%% @end
+%%
+%% FIXME: do a proper error/exception handling if the value of the option does 
+%%        not make sense (e.g. something other than an atom)
+%%
+-spec start(Options::[{register, atom()}|verbose]) -> {ok, Pid::pid()} | ignore | {error, Error::term()}.
+%% --------------------------------------------------------------------
+start(Options) ->
+	RegisterName = teu_application:opt(register, Options),
+    case RegisterName of
+		true ->      gen_server:start({local, ?MODULE}, ?MODULE, [Options], []);
+		false ->     gen_server:start(?MODULE, [Options], []); 
+        RegisterName ->  
+           if 
+            is_atom(RegisterName) -> gen_server:start({local, RegisterName}, ?MODULE, [Options], []);
+            true -> gen_server:start(?MODULE, [Options], [])
+		   end
+    end.
+
 
 %% last_message/1
 %% --------------------------------------------------------------------
 %% @doc return the last message recieved by the mock.
 %% @end
-%% --------------------------------------------------------------------
 -spec last_message(Pid::pid()) -> term().
+%% --------------------------------------------------------------------
 last_message(Pid) ->
     gen_server:call(Pid, get_last).
 
@@ -63,10 +90,57 @@ last_message(Pid) ->
 %% --------------------------------------------------------------------
 %% @doc return the messages retrieved by the mock, as a list, last message first.
 %% @end
-%% --------------------------------------------------------------------
 -spec message_stack(Pid::pid()) -> term().
+%% --------------------------------------------------------------------
 message_stack(Pid) ->
     gen_server:call(Pid, get_stack).
+
+%% is_verbose/1
+%% --------------------------------------------------------------------
+%% @doc return true if verbose message reporting is activated, false
+%% otherwise. 
+%% @end
+-spec is_verbose(Pid::pid()) -> true | false.
+%% --------------------------------------------------------------------
+is_verbose(Pid) ->
+    gen_server:call(Pid, is_verbose).
+
+%% wait_for_msg/1
+%% --------------------------------------------------------------------
+%% @doc block until any message is recieved by the async_mock process.
+%% @end
+%% --------------------------------------------------------------------
+-spec wait_for_msg(Pid) -> {ok, Message} | {error, timeout} when
+     Pid :: pid(),
+     Message :: term().
+%% --------------------------------------------------------------------
+wait_for_msg(Pid) -> wait_for_msg(Pid, fun(_) -> true end).
+
+%% wait_for_msg/2
+%% --------------------------------------------------------------------
+%% @doc block until a message matching the argument is recieved by the
+%% async_mock process.
+%%
+%% The message is matched using a predicate that will return true for 
+%% acceptable messages, false otherwise.
+%% @end
+%% --------------------------------------------------------------------
+-spec wait_for_msg(Pid, FilterPred) -> {ok, Message} | {error, timeout} when
+     Pid :: pid(),
+     FilterPred :: fun(),
+     Message :: term().
+%% --------------------------------------------------------------------
+wait_for_msg(Pid, FilterPred) ->
+	gen_server:cast(Pid, {teu_async_mock_internal_wait_for_msg, FilterPred, self()}),
+	
+    receive
+	    {received_msg, Msg} -> 
+			gen_server:cast(Pid, {teu_async_mock_internal_received_msg, FilterPred, self()}),
+			{ok, Msg}
+    after
+        %% wait 3 seconds
+		3000 -> {error, timeout} 
+    end.
 
 %% stop/1
 %% --------------------------------------------------------------------
@@ -91,7 +165,8 @@ stop(Pid) -> gen_server:cast(Pid, stop).
 %% --------------------------------------------------------------------
 init([Options]) ->
 	Verbose = teu_application:opt(verbose, Options),
-
+%% 	?debugFmt("Options: ~p, opt_verbose: ~p~n",[Options, Verbose]),
+	
     {ok, #state{opt_verbose = Verbose}}.
 
 %% --------------------------------------------------------------------
@@ -112,6 +187,10 @@ handle_call(get_stack, _From, State) ->
 %%     ?debugFmt("teu_async_mock:handle_call called for: get_last",[]),
     {reply, State#state.messageStack, State};
 
+handle_call(is_verbose, _From, State) ->
+%%     ?debugFmt("teu_async_mock:handle_call called for: is_verbose",[]),
+    {reply, State#state.opt_verbose, State};
+
 handle_call(Request, _From, State) ->
 %%     ?debugFmt("unknown message: ~p", [Request]),
     {reply, {error, {unknown_message, Request}}, State}.
@@ -126,14 +205,32 @@ handle_call(Request, _From, State) ->
 handle_cast(stop, State) ->
     {stop, normal, State};
 
+handle_cast({teu_async_mock_internal_wait_for_msg, FilterPred, ObserverPid}, State) -> 
+	{noreply, State#state{observer=ObserverPid, observerPred=FilterPred}};
+
+handle_cast({teu_async_mock_internal_received_msg, _FilterPred, _ObserverPid}, State) -> 
+	{noreply, State#state{observer=none}};
+
 handle_cast(Msg, State) ->
 	
 	case State#state.opt_verbose of
-		true -> io:format("have seen message: ~p",[Msg]);
+		true -> io:format("have seen message: ~p\n",[Msg]);
 		_    -> ok
     end,
 
     OldStack = State#state.messageStack,
+	
+	%% notify observer
+    case State#state.observer of
+		Pid when is_pid(Pid) -> 
+			ObserverPred = State#state.observerPred,
+			case ObserverPred(Msg) of
+				true -> Pid ! {received_msg, Msg};
+                _ -> ok
+				end;
+	    _ -> ok
+	end,
+
     {noreply, State#state{lastMessage = Msg, messageStack = [Msg|OldStack]}}.
 
 
--- test/teu_async_mock_tests.erl
+++ test/teu_async_mock_tests.erl
@@ -15,27 +15,26 @@
 %%
 %% Exported Functions
 %%
--export([]).
+-export([verbose_mock_runner/1]).
+
+-record(fun_call, {module = undefined, function = undefined, args = []}).
 
 %%
 %% Fixtures
 %%
 
-info_test_() ->
-    { setup, fun() -> ok end,
-      fun() -> ?debugFmt("~n############################################~n      starting ~p~n############################################~n  ", [?MODULE]) end }.
+%% info_test_() ->
+%%     { setup, fun() -> ok end,
+%%       fun() -> ?debugFmt("~n############################################~n      starting ~p~n############################################~n  ", [?MODULE]) end }.
 
 initial_test_() ->
     { "some initial tests",
       setup,
 
       fun() ->
-              %%     application:start(sasl),
               ok
       end,
-
       fun(_Args) ->
-              %%     application:stop(sasl),
               ok
       end,
       fun(_Foo) -> [
@@ -47,11 +46,123 @@ initial_test_() ->
                    ]
       end }.
 
+verbose_output_test_() ->
+	{"setting the verbose option shall write an incoming message to standard out",
+	 setup,
+	 fun() ->
+			 ok
+	 end,
+	 fun(_Args) -> 
+			 ok 
+	 end,
+	 fun(_Args) ->
+			 [
+			  ?_test(test_verbose_output())
+			 ]
+	 end
+	}.
+
+wait_for_message_test_() ->
+    { "the calling process shall block until it the mock recieves the right message", 
+	  setup, 
+	  fun() -> 
+			  {ok, MPid} = teu_async_mock:start([verbose]),
+			  MPid
+	  end,
+      fun(MPid) ->
+			  teu_async_mock:stop(MPid)
+	  end,
+	  fun(MPid) -> [
+					?_test(test_wait_for_any_msg(MPid)),
+					?_test(test_wait_for_msg_match(MPid))
+                    ]
+	  end }.
 
 
 %%
 %% Local Functions
 %%
+
+test_verbose_output() ->
+	?assertCmdOutput("have seen message: message\n",
+					  get_run_functions_command([#fun_call{module = teu_async_mock_tests, 
+														   function = verbose_mock_runner, 
+														   args = [message]}])),
+	ok.
+
+verbose_mock_runner(Msg) ->
+	{ok, Pid} = teu_async_mock:start([verbose]),
+	gen_server:cast(Pid, Msg),
+	teu_async_mock:stop(Pid),
+	
+	ok.
+
+get_run_functions_command(FunCalls) ->
+	get_run_text_command(create_function_call_sequence_string(FunCalls)).
+
+get_run_text_command(Text) ->
+	ErlPrefix = "erl -pa .eunit -run -eval \"",
+	ErlSuffix = "\" -s init stop -noshell",
+	Command = ErlPrefix ++ escape_quotes(Text) ++ ErlSuffix,
+%% 	?debugFmt("~p~n", [Command]),
+	Command.
+
+create_function_call_sequence_string(FunCalls) ->
+	string:join(lists:map(fun create_function_call_string/1, FunCalls), ", ") ++ ".".
+
+create_function_call_string(#fun_call{module = Module, function = Function, args = Arguments}) ->
+	QualifiedFunctionName = atom_to_list(Module) ++ ":" ++ atom_to_list(Function),
+	ArgumentsString = get_arguments_string(Arguments),
+	QualifiedFunctionName ++ "(" ++ ArgumentsString ++ ")".
+
+get_arguments_string(Arguments) ->
+	ArgumentTexts = [lists:flatten(io_lib:format("~p", [Arg])) || Arg <- Arguments],
+	string:join(ArgumentTexts, ", ").
+
+escape_quotes(String) ->
+	lists:flatten(lists:map(fun
+							   ($\") -> "\\\"";
+							   (X) -> X
+							end, String)).
+
+test_wait_for_msg_match(MPid) ->
+
+	%% create another process to actually make the call, so we can start the
+    %% actual function to wait for the message.
+	_TmpPid = spawn(fun() -> 
+							timer:sleep(500), 
+							gen_server:cast(MPid, {an_invalid_message, notme}), 
+							gen_server:cast(MPid, {a_valid_message, 3}) 
+					end),
+	
+%% 	?debugFmt("mock PId: ~p~n",[MPid]),
+	
+	?assertMatch({ok, {a_valid_message, _V}}, 
+				 teu_async_mock:wait_for_msg(MPid, 
+                                             fun(Arg) -> 
+                                                case Arg of 
+                                                   {a_valid_message, _} -> true;
+                                                   _ -> false
+                                                end 
+                                             end)),
+	
+	ok.
+
+test_wait_for_any_msg(MPid) ->
+
+	%% create another process to actually make the call, so we can start the
+    %% actual function to wait for the message.
+	_TmpPid = spawn(fun() -> 
+							timer:sleep(500), 
+							gen_server:cast(MPid, a_valid_message) 
+					end),
+	
+%% 	?debugFmt("mock PId: ~p~n",[MPid]),
+	
+	?assertMatch({ok, _Msg}, teu_async_mock:wait_for_msg(MPid)),
+	
+	ok.
+
 test_send_message() ->
 	Res = teu_async_mock:start_link([]),
 	?assertMatch({ok, _Pid}, Res),
@@ -82,7 +193,7 @@ test_register() ->
     ?assertEqual(Pid1, whereis(teu_async_mock)),
 
     teu_async_mock:stop(Pid1),
-    sleep(100),
+    timer:sleep(100),
     ?assertEqual(undefined, whereis(teu_async_mock)),
 
     {ok, Pid2} = teu_async_mock:start_link([{register, my_proc_name}]),
@@ -90,7 +201,7 @@ test_register() ->
     ?assertEqual(Pid2, whereis(my_proc_name)),
 
     teu_async_mock:stop(Pid2),
-    sleep(100),
+    timer:sleep(100),
     ?assertEqual(undefined, whereis(my_proc_name)),
 
     ok.
@@ -104,6 +215,8 @@ test_verbose() ->
 	gen_server:cast(Pid, test_message),
 	
 	?assertEqual(test_message, teu_async_mock:last_message(Pid)),
+	
+	?assert(teu_async_mock:is_verbose(Pid)),
 
 	teu_async_mock:stop(Pid),
 
@@ -116,9 +229,3 @@ test_errenous_call_msg() ->
 	
 	teu_async_mock:stop(Pid),
 	ok.
-
-%% sleep for number of miliseconds
-sleep(T) ->
-  receive 
-    after T -> ok 
-  end.
