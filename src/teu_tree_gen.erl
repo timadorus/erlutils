f919fcfc51a684565d62ae8705be81b5bfb876a3
Lutz Behnke <lutz.behnke@gmx.de>
2016-12-07 16:03:55 +0100

    still snapsho

--- src/teu_tree_gen.erl
+++ src/teu_tree_gen.erl
@@ -6,14 +6,11 @@
 
 -module(teu_tree_gen).
 
-
--include_lib("eunit/include/eunit.hrl").
-
 %% init_ctrl/1
 %% --------------------------------------------------------------------
 %% @ doc initialize a generator controler.
 %% @ end
--callback init_ctrl(Args :: [term()]) -> {ok, State :: term()} | {error, Reason :: term()}.
+-callback init_ctrl(Args :: [term()]) -> {ok, Root :: term(), State :: term()} | {error, Reason :: term()}.
 %% --------------------------------------------------------------------
 
 %% init_gen/1
@@ -23,36 +20,51 @@
 -callback init_gen(Args :: [term()]) -> {ok, State :: term()} | {error, Reason :: term()}.
 %% --------------------------------------------------------------------
 
-%% gen_node/2
+%% gen_nodes/2
 %% --------------------------------------------------------------------
-%% @ doc generate the next node.
+%% @ doc generate the next nodes.
+%% This may generate any number of new nodes. Usually based on the type of tree
+%% (e.g. an octree usually will have eight new nodes) and the information in Info.
 %%
 %% @param Parent parent node information
 %% @param Info additional information provided by the controler to the generator.
+%% @param
 %% @ end
--callback gen_node(Parent :: term(), Info :: term()) ->
-        {ok, Node :: term(), State :: term()} | {error, Reason :: term()}.
+-callback gen_node(Parent :: term(), Info :: term(), GenState :: term()) ->
+        {ok, Nodes :: [term()], State :: term()} | {error, Reason :: term()}.
 %% --------------------------------------------------------------------
 
-%% -record(node_info, { state :: pending | complete | leaf
-%%                    , data :: term()  %% set by the generator
-%%                    }).
+%% is_final/1
+%% --------------------------------------------------------------------
+%% @doc determine whether a node is final or needs more processing.
+%% --------------------------------------------------------------------
+-callback is_final(Node :: term()) -> true | false.
+%% --------------------------------------------------------------------
+
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-include_lib("eunit/include/eunit.hrl").
+
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([start/3]).
+-export([start/1, start_link/1]).
 
-%% start/3
+%% start/1
 %% --------------------------------------------------------------------
-%% @doc start generator systm
+%% @doc start generator system.
+%%
 %% this will start at least two processes (one controler and one worker),
 %% but may start an arbitrary number more, depending on the options.
 %%
 %% Options are:
 %% <dl>
-%% <dt><code>generator</code></dt><dd>Module implementing the generator (atom(), Default: )<dd>
+%% <dt><code>generator</code></dt><dd>Module implementing the generator (<code>atom()</code>)<dd>
 %% <dt><code>gen_args</code></dt><dd>Arguments for the generator/controler (Default: [])<dd>
+%% <dt><code>tree_impl</code></dt><dd>Module implementing the tree (<code>atom()</code>, Default: teu_simple_tree_impl)<dd>
+%% <dt><code>tree_args</code></dt><dd>Arguments for the tree implementation (Default: [])<dd>
 %% </dl>
 %%
 %% @param Options a map of the configuration
@@ -63,15 +75,154 @@
 %% @param GenArgs additional arguments for the controler.
 %% @param TreeImpl  name of the module implementing the tree.
 %% @param TreeArgs additional arguemtns of the tree implementation
+%% @returns the pid of the control wrapper.
 %% @param
--spec start(GenModule :: atom(), GenArgs :: term(),
-            TreeImpl :: atom(), TreeArgs :: term(),
-            Options :: #{}).
+-spec start(Options :: #{}) ->
+         {ok, pid()} | {error, Reason :: term()}.
 %% --------------------------------------------------------------------
-start()
+start(Options) -> gen_server:start(?MODULE, Options, []).
+
+
+%% start_link/1
+%% --------------------------------------------------------------------
+%% @doc start generator system.
+%%
+%% same as @see start/1, but will link the resultant process.
+%% @end
+-spec start_link(Options :: #{}) ->
+         {ok, pid()} | {error, Reason :: term()}.
+%% --------------------------------------------------------------------
+start_link(Options) -> gen_server:start_link(?MODULE, Options, []).
+
+
+
+%% ====================================================================
+%% Behavioural functions
+%% ====================================================================
+-record(tree_gen_info, { tree_impl = teu_simple_tree_impl   :: atom()
+                       , generator                          :: atom()
+                       , root_node                          :: term()
+                       , control_state                      :: term()
+                       , gen_state                          :: term()
+                       }).
+
+%% init/1
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:init-1">gen_server:init/1</a>
+-spec init(Args :: term()) -> Result when
+    Result :: {ok, State}
+            | {ok, State, Timeout}
+            | {ok, State, hibernate}
+            | {stop, Reason :: term()}
+            | ignore,
+    State :: term(),
+    Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+init(Options) ->
+    TreeImpl = teu_application:opt(tree_impl, Options, teu_simple_tree_impl),
+    TreeArgs = teu_application:opt(tree_args, Options, []),
+
+    Generator =
+        case  teu_application:opt(generator, Options, undefined) of
+            undefined ->
+                throw(generator_module_not_defined);
+            Gen when is_atom(Gen) ->
+                Gen;
+            _ ->
+                throw(generator_module_not_defined)
+        end,
+
+    GenArgs = teu_application:opt(tree_args, Options, []),
+    {RootNode, CtrlState} =
+        case Generator:init_gen(GenArgs) of
+            {ok, RN, CS} -> {RN, CS};
+            {error, Reason} -> throw({gen_init_failed, Reason})
+        end,
+
+    {ok, #tree_gen_info{tree_impl = TreeImpl, generator = Generator,
+                        root_node = RootNode,
+                        control_state = CtrlState, gen_state = GenState}}.
+
+
+%% handle_call/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_call-3">gen_server:handle_call/3</a>
+-spec handle_call(Request :: term(), From :: {pid(), Tag :: term()}, State :: term()) -> Result when
+    Result :: {reply, Reply, NewState}
+            | {reply, Reply, NewState, Timeout}
+            | {reply, Reply, NewState, hibernate}
+            | {noreply, NewState}
+            | {noreply, NewState, Timeout}
+            | {noreply, NewState, hibernate}
+            | {stop, Reason, Reply, NewState}
+            | {stop, Reason, NewState},
+    Reply :: term(),
+    NewState :: term(),
+    Timeout :: non_neg_integer() | infinity,
+    Reason :: term().
+%% ====================================================================
+handle_call(Request, From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+
+%% handle_cast/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_cast-2">gen_server:handle_cast/2</a>
+-spec handle_cast(Request :: term(), State :: term()) -> Result when
+    Result :: {noreply, NewState}
+            | {noreply, NewState, Timeout}
+            | {noreply, NewState, hibernate}
+            | {stop, Reason :: term(), NewState},
+    NewState :: term(),
+    Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_cast(Msg, State) ->
+    {noreply, State}.
+
+
+%% handle_info/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_info-2">gen_server:handle_info/2</a>
+-spec handle_info(Info :: timeout | term(), State :: term()) -> Result when
+    Result :: {noreply, NewState}
+            | {noreply, NewState, Timeout}
+            | {noreply, NewState, hibernate}
+            | {stop, Reason :: term(), NewState},
+    NewState :: term(),
+    Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+
+%% terminate/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:terminate-2">gen_server:terminate/2</a>
+-spec terminate(Reason, State :: term()) -> Any :: term() when
+    Reason :: normal
+            | shutdown
+            | {shutdown, term()}
+            | term().
+%% ====================================================================
+terminate(_Reason, _State) ->
+    ok.
+
+
+%% code_change/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:code_change-3">gen_server:code_change/3</a>
+-spec code_change(OldVsn, State :: term(), Extra :: term()) -> Result when
+    Result :: {ok, NewState :: term()} | {error, Reason :: term()},
+    OldVsn :: Vsn | {down, Vsn},
+    Vsn :: term().
+%% ====================================================================
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+
 
 
--- test/teu_iterative_deep_tree_gen.erl
+++ test/teu_iterative_deep_tree_gen.erl
@@ -1,5 +1,5 @@
 %% @author sage
-%% @doc @todo Add description to teu_iterative_deep_tree_gen.
+%% @doc implement an iterative deeping of tree.
 
 
 -module(teu_iterative_deep_tree_gen).
@@ -15,7 +15,7 @@
 %% ====================================================================
 %% Behavior functions
 %% ====================================================================
--export([init_ctrl/1, init_gen/1, gen_node/2]).
+-export([init_ctrl/1, init_gen/1, gen_nodes/3, is_final/1]).
 
 -record(ctrl_state, { state :: pending | complete | leaf
                    , data :: term()  %% set by the generator
@@ -29,9 +29,10 @@
 %% --------------------------------------------------------------------
 %% @ doc initialize a generator control.
 %% @ end
--spec init_ctrl(Args :: [term()]) -> {ok, State :: term()} | {error, Reason :: term()}.
+-spec init_ctrl(Args :: [term()]) ->
+          {ok, Root :: term(), State :: term()} | {error, Reason :: term()}.
 %% --------------------------------------------------------------------
-init_ctrl(_Args) -> {ok, #ctrl_state{}}.
+init_ctrl(_Args) -> {ok, root, #ctrl_state{}}.
 
 
 %% init_gen/1
@@ -40,18 +41,27 @@ init_ctrl(_Args) -> {ok, #ctrl_state{}}.
 %% @ end
 -spec init_gen(Args :: [term()]) -> {ok, State :: term()} | {error, Reason :: term()}.
 %% --------------------------------------------------------------------
+init_gen(_Args) -> #node_state{}.
 
-%% gen_node/2
+%% gen_nodes/2
 %% --------------------------------------------------------------------
 %% @ doc generate the next node.
 %%
 %% @param Parent parent node information
 %% @param Info additional information provided by the controler to the generator.
 %% @ end
--spec gen_node(Parent :: term(), Info :: term()) ->
-        {ok, Node :: term(), State :: term()} | {error, Reason :: term()}.
+-spec gen_nodes(Parent :: term(), Info :: term(), GenState :: term()) ->
+        {ok, Nodes :: [term()], State :: term()} | {error, Reason :: term()}.
 %% --------------------------------------------------------------------
+gen_nodes(_Parent, _Info, GenState) -> {ok, [], GenState}.
 
+%% is_final/1
+%% --------------------------------------------------------------------
+%% @doc determine whether a node is final or needs more processing.
+%% --------------------------------------------------------------------
+-spec is_final(Node :: term()) -> true | false.
+%% --------------------------------------------------------------------
+is_final(_Node) -> false.
 
 %% ====================================================================
 %% Internal functions
